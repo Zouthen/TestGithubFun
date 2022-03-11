2b21511863b12c69782626479875998657ae5dae
Zouthen <mich5n69@stud.kea.dk>
2022-03-11 09:55:26 +0100

    Hello again, whats up

--- src/fun.java
+++ src/fun.java
@@ -1,5 +1,7 @@
 public class fun {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Hello again");
+        System.out.println("Whats up");
     }
 }
