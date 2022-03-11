f21cbc10049fe31af1e367ac25f481c41c71d8e5
AugustG01 <augu8204@stud.kea.dk>
2022-03-11 09:53:52 +0100

    You, Hi

--- src/fun.java
+++ src/fun.java
@@ -1,5 +1,7 @@
 public class fun {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("You");
+        System.out.println("Hi");
     }
 }
